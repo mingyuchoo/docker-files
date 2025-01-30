80c556d30a6ac146e12b80b8505d012aeda7f5ad
mingyuchoo <mingyuchoo@gmail.com>
2025-01-30 16:19:52 +0900

    added

---
+++ sbcl/hunchentoot-web-app/src/main.lisp
@@ -0,0 +1,22 @@
+;; Load Quicklisp
+(load "~/quicklisp/setup.lisp")
+
+;; Load Hunchentoot server
+(ql:quickload "hunchentoot")
+
+;; Define packages
+(defpackage :my-web-server
+  (:use :cl :hunchentoot))
+(in-package :my-web-server)
+
+;; Define simple handler
+(define-easy-handler (hello :uri "/") ()
+  (setf (hunchentoot:content-type*) "text/plain")
+  "Hello, World!")
+
+;; Run web server
+(defvar *my-server* (hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :address "0.0.0.0" :port 4000)))
+
+(format t "Server started on http://0.0.0.0:4000~%")
+
+(loop (sleep 60))
